4826481e2a9929446f4fa02cc04e5b117f0efaa7
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-04 12:50:20 -0700

    Initial commit.

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+//404 response
+function send404(response) {
+  response.writeHead(404, {"Content-Type": "text/plain"});
+  response.write("Error 404: There's no page here.");
+  response.end();
+};
+
+function onRequest(request, response) {
+  if (request.method == 'GET' && request.url == '/') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream("./index.html").pipe(response);
+  } else {
+    send404(response);
+  }
+};
+
+http.createServer(onRequest).listen(3000);
+console.log("server is now running")
